980a245dffd873171a2752a3b2831632c677bf2d
Joakimglodedata <joakim.glodedata@gmail.com>
2025-06-16 13:59:30 +0200

    Kult

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 // setter opp consttanter for dei mest brukte funksjonene så eg slipper å skrive ut heile kvar gang
 const sel = (selector) => document.querySelector(selector);
+// Ser at å sette opp console.log på denne måten får javascript til å gjøre alle consol kommandoer på denne linjen, plagsomt men levbart
 const cons = (selector) => console.log(selector);
-// sel(cons(".flexCenterRow"))
-// console.log(document.querySelector(".flexCenterRow"))
-// Tar vekk form funksjoner så siden ikkje laster seg selv inn igjen
+
+// Tar vekk form funksjoner så nettsiden ikkje laster seg selv inn igjen
 sel("#form").addEventListener('submit', function (e) {
     e.preventDefault()});
 
 const fruitBowl = []
+cons(fruitBowl.length)
 
 
-fruitBowl.push("Orange")
-fruitBowl.push("Banana")
-fruitBowl.push
 
 //Setter opp konstanter som blir brukt videre
 const shoppingItem = sel("#shoppingItem")
@@ -22,15 +20,15 @@ const unshiftB = sel("#unshiftB")
 const shiftB = sel("#shiftB")
 const displayItems = sel("#displayItems")
 const restart = sel("#restart")
-
-displayItems.textContent = fruitBowl.join(", ")
+const displayEmpty = sel("#displayEmpty")
 
 
 restart.addEventListener("click", function () {
+// Blir kvitt siste elementet fra fruitBowl arrayen og kjører så lenge den har ein lengde over 0
+// Har ingen failsafe for å hindre uendelige loops, men ser ikkje korleis det kan hende her så bare håper eg har rett :)
     while (fruitBowl.length > 0) {
         fruitBowl.pop()
         cons(fruitBowl.length)
-        displayItems.textContent = fruitBowl.join(", ")
         displayEmptyF ()
     }
 })
@@ -39,7 +37,6 @@ pushB.addEventListener("click", function () {
     const shoppingItemValue = shoppingItem.value
     if (shoppingItemValue) {
         fruitBowl.push(shoppingItemValue)
-        displayItems.textContent = fruitBowl.join(", ")
         shoppingItem.value = ""
         displayEmptyF ()
     } else {
@@ -49,7 +46,6 @@ pushB.addEventListener("click", function () {
 
 popB.addEventListener("click", function () {
     fruitBowl.pop()
-    displayItems.textContent = fruitBowl.join(", ")
     displayEmptyF ()
 })
 
@@ -57,7 +53,6 @@ unshiftB.addEventListener("click", function () {
     const shoppingItemValue = shoppingItem.value
     if (shoppingItemValue) {
         fruitBowl.unshift(shoppingItemValue)
-        displayItems.textContent = fruitBowl.join(", ")
         shoppingItem.value = ""
         displayEmptyF ()
     } else {
@@ -71,12 +66,13 @@ shiftB.addEventListener("click", function () {
     displayEmptyF ()
 })
 
+// Med å ta vekk form funksjoner tok eg også vekk evnen til å presse enter, gjør det samme som push knappen men blir aktivert av å presse enter
+// Blir aktivert av kvart knappetrykk, sjekker om trykket var enter og om textfeltet er i bruk før den endrer noe
 document.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
+    if (event.key === 'Enter' && document.activeElement === shoppingItem) {
         const shoppingItemValue = shoppingItem.value
         if (shoppingItemValue) {
             fruitBowl.push(shoppingItemValue)
-            displayItems.textContent = fruitBowl.join(", ")
             shoppingItem.value = ""
             displayEmptyF ()
         } else {
@@ -85,8 +81,9 @@ document.addEventListener('keydown', function(event) {
     }
 })
 
+// Oppdaterer displayItems og sjekker om fruitBowl er tom for å endre verdien til displayEmpty
 function displayEmptyF () { 
-    const displayEmpty = sel("#displayEmpty")
+    displayItems.textContent = fruitBowl.join(", ")
     if (fruitBowl.length === 0 ) { 
         displayEmpty.textContent = "Listen er tom"
         cons("if fungerer")
@@ -95,5 +92,8 @@ function displayEmptyF () {
         cons("else fungerer")
     }
 }
+
+fruitBowl.push("Orange")
+fruitBowl.push("Banana")
 
 displayEmptyF ()
